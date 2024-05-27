c6d95d6870b5efcf23f469486198c0cf1bb31ae0
Sotp_kat <joram@vanverseveld.org>
2024-05-27 17:12:59 +0200

    NOW UPLOAD THIS SHIT AS WELL

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,16 @@
+#include "keypad.h"
+#include "rfid.h"
+#include "receipt_printer.h"
+
+void setup() {
+  Serial.begin(460800);
+  Serial.setTimeout(50);
+  initRfid();
+  initPrinter();
+}
+
+void loop() {
+  getRfid();
+  getKeypad();
+  printReceipt();
+}
